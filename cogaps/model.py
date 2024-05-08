9d2d489843dac2a3b38cee9a1acd6700a590347d
kwoyshner <kwoyshn1@jh.edu>
2024-05-08 09:30:56 -0400

    random initialization A_mean, pos contraints scale params

--- cogaps/model.py
+++ cogaps/model.py
@@ -1,12 +1,11 @@
-import torch
 import pyro
 import pyro.distributions as dist
-from torch import nn
+import torch
+from pyro.distributions import lkj
 from pyro.nn import PyroModule
 from pyro.nn.module import PyroParam
+from torch import nn
 from torch.nn.functional import softplus
-from pyro.distributions import lkj
-
 
 #%% Enable Validations
 pyro.enable_validation(True)
@@ -29,7 +28,7 @@ pyro.enable_validation(True)
 #         P = pyro.sample("P", dist.Exponential(rate=P_scale).to_event(1))
 
 #     prediction = torch.matmul(A, P)
-    
+
 #     with pyro.plate("data", num_genes):
 #         #pyro.sample("obs", dist.Poisson(prediction), obs=D.T)
 #         # Use Negative Binomial distribution instead of Poisson
@@ -51,7 +50,7 @@ class CoGAPSModel(PyroModule):
         self.initial_scale = 0.1 * self.D_gene_means
         self.init_vals = self.initial_scale.unsqueeze(-1)
         self.init_vals = self.init_vals.expand(-1, self.num_patterns)
-        
+
         # Define parameters
         #self.A_scale = PyroParam(torch.full((self.num_genes, self.num_patterns), self.initial_scale, device=self.device), constraint=dist.constraints.positive)
         #self.A_scale = PyroParam(self.init_vals.to(device), constraint=dist.constraints.positive)
@@ -71,7 +70,7 @@ class CoGAPSModel(PyroModule):
 
         with pyro.plate("data", self.num_genes):
             pyro.sample("obs", dist.Normal(prediction, torch.ones_like(prediction)).to_event(1), obs=D)
-        
+
         return prediction
 
 # Starting below from scratch
@@ -93,34 +92,34 @@ class ProbNMFModel(PyroModule):
         self.init_vals = self.init_vals.expand(-1, self.num_patterns)
         #Clamp to min of 0.01
         self.init_vals = torch.clamp(self.init_vals, min=0.01)
-        
-        
+
+
         # Define parameters
-        #self.A_mean = PyroParam(torch.rand((self.num_genes, self.num_patterns), device=self.device),constraint=dist.constraints.positive)
-        self.A_mean = PyroParam(self.init_vals.to(device), constraint=dist.constraints.positive)
-        self.A_scale = PyroParam(torch.ones((self.num_genes, self.num_patterns), device=self.device))
-        
+        self.A_mean = PyroParam(torch.rand((self.num_genes, self.num_patterns), device=self.device),constraint=dist.constraints.positive)
+        #self.A_mean = PyroParam(self.init_vals.to(device), constraint=dist.constraints.positive)
+        self.A_scale = PyroParam(torch.ones((self.num_genes, self.num_patterns), device=self.device),constraint=dist.constraints.positive)
+
         self.P_mean = PyroParam(torch.rand((self.num_patterns, self.num_samples), device=self.device),constraint=dist.constraints.positive)
-        self.P_scale = PyroParam(torch.ones((self.num_patterns, self.num_samples), device=self.device))
-        
+        self.P_scale = PyroParam(torch.ones((self.num_patterns, self.num_samples), device=self.device),constraint=dist.constraints.positive)
+
         #self.D_mean = PyroParam(torch.tensor(D.mean(), device=self.device))
-        
+
     def forward(self, D):
         # Priors
         genes_plate = pyro.plate("Genes", self.num_genes, dim=-2)
         patterns_plate = pyro.plate("Patterns", self.num_patterns, dim=-3)
-        
+
         with genes_plate:
             A = pyro.sample("A", dist.Normal(self.A_mean, self.A_scale).to_event(1))
-            
+
         with patterns_plate:
             P = pyro.sample("P", dist.Normal(self.P_mean, self.P_scale).to_event(1))
-            
+
         prediction = torch.matmul(A, P)
         # Likelihood
         #with pyro.plate("data", self.num_genes):
         with genes_plate:
             with patterns_plate:
-                
+
                 pyro.sample("D", dist.Normal(softplus(prediction),torch.ones_like(prediction)).to_event(1), obs=D)
         return prediction
