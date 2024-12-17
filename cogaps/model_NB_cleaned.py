2e4b19af1533157b1cf20a85eae451972ae08da1
kwoyshner <kwoyshn1@jh.edu>
2024-12-17 10:14:12 -0500

    First attempts at cleaning

---
+++ cogaps/model_NB_cleaned.py
@@ -0,0 +1,148 @@
+#%%
+import pyro
+import pyro.distributions as dist
+import torch
+from pyro.nn import PyroModule
+from pyro.nn.module import PyroParam
+from torch import nn
+from torch.nn.functional import softplus
+import torch
+import pyro
+import pyro.distributions as dist
+from pyro.nn import PyroModule
+from pyro.optim import Adam
+from pyro.infer import SVI, Trace_ELBO
+import pyro.poutine as poutine
+import matplotlib.pyplot as plt
+import numpy as np
+import pandas as pd
+import anndata as ad
+
+
+
+#%% Enable Validations
+pyro.enable_validation(True)
+
+#%%
+class GammaMatrixFactorization(PyroModule):
+    def __init__(self,
+                 num_genes,
+                 num_patterns,
+                 num_samples,
+                 NB_probs = 0.5,
+                 device=torch.device('cpu')
+                 #init_method="mean", # Options: (["mean", "svd", None]):
+            ):
+        super().__init__()
+
+        self.num_genes = num_genes
+        self.num_patterns = num_patterns
+        self.num_samples = num_samples
+        self.NB_probs = NB_probs
+        self.device = device
+
+        print(f"Using {self.device}")
+
+        #### Matrix A is patterns x genes ####
+        self.loc_A = PyroParam(torch.rand(self.num_patterns, self.num_genes, device=self.device), constraint=dist.constraints.positive)
+        self.scale_A = PyroParam(torch.ones(self.num_patterns, self.num_genes, device=self.device),constraint=dist.constraints.positive)
+
+        #### Matrix P is samples x patterns ####
+        self.loc_P = PyroParam(torch.rand(self.num_samples, self.num_patterns, device=self.device),constraint=dist.constraints.positive)
+        self.scale_P = PyroParam(torch.ones(self.num_samples, self.num_patterns, device=self.device),constraint=dist.constraints.positive)
+
+    def forward(self, D):
+        # Nested plates for pixel-wise independence?
+        with pyro.plate("patterns", self.num_patterns, dim = -2):
+            with pyro.plate("genes", self.num_genes, dim = -1):
+                A = pyro.sample("A", dist.Gamma(self.loc_A, self.scale_A)) # sample A from Gamma
+
+        # Nested plates for pixel-wise independence?
+        with pyro.plate("samples", self.num_samples, dim=-2):
+            with pyro.plate("patterns_P", self.num_patterns, dim = -1):
+                P = pyro.sample("P", dist.Gamma(self.loc_P, self.scale_P)) # sample P from Gamma
+
+        # Matrix D_reconstucted is samples x genes; calculated as the product of P and A
+        #D_reconstructed = softplus(torch.matmul(P, A))  # (samples x genes)
+        D_reconstructed = torch.matmul(P, A)  # (samples x genes)
+        self.D_reconstructed = D_reconstructed # save D_reconstructed
+
+        pyro.sample("D", dist.NegativeBinomial(D_reconstructed, probs=self.NB_probs).to_event(2), obs=D) ## Changed distribution to NegativeBinomial
+
+def guide(D):
+    pass
+
+
+#%% Plotting function
+
+# make sure to change CCF coords -1*y
+
+def plot_grid(patterns, coords, nrows, ncols, savename = None):
+    fig, axes = plt.subplots(nrows,ncols, figsize=(ncols*4, nrows*4))
+    num_patterns = patterns.shape[1]
+    x, y = coords['x'], coords['y']
+    i = 0
+    for r in range(nrows):
+        for c in range(ncols):
+            if i < num_patterns:
+                p5 = np.percentile(patterns[:,i], 5)
+                p95 = np.percentile(patterns[:,i], 95)
+
+                #pattern_min = np.min(patterns[:, i])
+                #pattern_max = np.max(patterns[:, i])
+                #alpha_values = 0.3 + (0.7 * (patterns[:, i] - pattern_min) / (pattern_max - pattern_min))
+                #axes[r,c].scatter(x, y, c=patterns[:,i], s=8,alpha=np.minimum(alpha_values, 1), vmin=p5, vmax=p95, cmap='viridis',edgecolors='none')
+                p = axes[r,c].scatter(x, y, c=patterns[:,i], s=6,alpha=0.7, vmin=p5, vmax=p95, cmap='viridis',edgecolors='none')
+                axes[r,c].set_yticklabels([])
+                axes[r,c].set_xticklabels([])
+                i += 1
+
+    if savename != None:
+        plt.savefig(savename)
+
+def plot_grid_NSF(patterns, coords, nrows, ncols, savename = None):
+    fig, axes = plt.subplots(nrows,ncols)
+    num_patterns = patterns.shape[1]
+    x, y = coords['x'], coords['y']
+    x_min, x_max = np.min(x), np.max(x)
+    y_min, y_max = np.min(y), np.max(y)
+    resolution = 0.114286
+    x_grid = np.arange(x_min, x_max + resolution, resolution)
+    y_grid = np.arange(y_min, y_max + resolution, resolution)
+
+    i = 0
+    for r in range(nrows):
+        for c in range(ncols):
+            if i < num_patterns:
+                # Initialize an empty grid (NaN-filled)
+                grid = np.full((len(y_grid), len(x_grid)), np.nan)
+                # Map the patterns to the grid; find nearest grid indices for each x, y pair
+                ix = np.searchsorted(x_grid, x) - 1
+                iy = np.searchsorted(y_grid, y) - 1
+                for j in range(len(patterns[:,i])):
+                    if 0 <= ix[j] < len(x_grid) and 0 <= iy[j] < len(y_grid):
+                        grid[iy[j], ix[j]] = patterns[j,i]
+                axes[r,c].imshow(grid, cmap='viridis')
+                i += 1
+    if savename != None:
+        plt.savefig(savename)
+
+
+def plot_correlations(true_vals, inferred_vals, savename = None):
+    true_vals_df = pd.DataFrame(true_vals)
+    true_vals_df.columns = ['True_' + str(x) for x in true_vals_df.columns]
+    inferred_vals_df = pd.DataFrame(inferred_vals)
+    inferred_vals_df.columns = ['Inferred_' + str(x) for x in inferred_vals_df.columns]
+    correlations = true_vals_df.merge(inferred_vals_df, left_index=True, right_index=True).corr().round(2)
+    plt.figure()
+    sns.clustermap(correlations.iloc[:true_vals.shape[1], true_vals.shape[1]:], annot=False, cmap='coolwarm', cbar=True, vmin=-1, vmax=1)
+    if savename != None:
+        plt.savefig(savename + '_correlations.png')
+
+
+def prep_anndata(ad_data, counts_layer = None):
+        if not(counts_layer):
+            print("Using raw counts from anndata.X")
+            D = torch.tensor(ad_data.X) ## RAW COUNT DATA
+        else:
+            print(f"Retrieve raw counts from {counts_layer} layer NOT YET IMPLEMENTED")
---
+++ run_model_NB_SVI.py
@@ -0,0 +1,211 @@
+#!/usr/bin/env -S python3 -u
+#%%
+import os
+from datetime import datetime
+import anndata as ad
+import matplotlib.pyplot as plt
+import numpy as np
+import pandas as pd
+import anndata as ad
+import pyro
+import pyro.optim
+import pyro.poutine as poutine
+import scanpy as sc
+import seaborn as sns
+import torch
+from pyro.infer.autoguide import \
+    AutoNormal  # , AutoDiagonalNormal, AutoMultivariateNormal, AutoLowRankMultivariateNormal
+from torch.utils.data import DataLoader, TensorDataset
+
+from cogaps.model_NB_cleaned import GammaMatrixFactorization, plot_grid, plot_correlations
+#from cogaps.utils import generate_structured_test_data, generate_test_data
+
+import random
+
+os.environ['PYTORCH_ENABLE_MPS_FALLBACK'] = '1'
+
+
+
+#%%
+
+#######################################################
+########### ALL USER DEFINED INPUTS HERE ##############
+#######################################################
+
+#### input data here, should be stored in anndata.X layer
+# If you have a numpy array of data try : data = ad.AnnData(array_data) # Kyla untested
+data = ad.read_h5ad('/disk/kyla/data/Zhuang-ABCA-1-raw_1.058_wMeta_wAnnotations_KW.h5ad') # samples x genes
+#data = data[data.obsm['atlas']['Isocortex']]
+
+D = torch.tensor(data.X) ## RAW COUNT DATA
+
+#### coords should be two columns named x and y
+coords = data.obs.loc[:,['x','y']] # samples x 2
+coords['y'] = -1*coords['y'] # specific for this dataset
+
+num_patterns = 20 # select num patterns
+num_steps = 10000 # Define the number of optimization steps
+
+device = None # options ['cpu', 'cuda', 'mps', etc]; if None: auto detect cpu vs gpu vs mps
+NB_probs = None # in range [0,1]; if None: use default of 1 - sparsity; this is the probs argument for NegativeBinomial for D
+
+#### output parameters
+outputDir = '/disk/kyla/projects/pyro_NMF/results/'
+savename = 'ABCA-1_wholeSlice' # output anndata will be saved in outputDir/savename.h5ad
+
+useTensorboard = True
+tensorboard_identifier = 'ABA_wholeSlice' # key added to tensorboard output name
+
+plot_dims = [5, 4] # rows x columns should be > num patterns; this is for plotting
+
+
+#### model parameters
+optimizer = pyro.optim.Adam({"lr": 0.1, "eps":1e-08}) # Use the Adam optimizer
+loss_fn = pyro.infer.Trace_ELBO() # Define the loss function
+draw_model = None # None or name for output file to create pdf diagram of pyro model
+
+
+
+#%%
+# Clear Pyro's parameter store
+pyro.clear_param_store()
+
+if device == None:
+    if torch.backends.mps.is_available():
+        device = torch.device('mps')
+    elif torch.cuda.is_available():
+        device = torch.device('cuda')
+    else:
+        device = torch.device('cpu')
+#device = device
+
+print(f"Using {device}")
+D = D.to(device)
+
+if NB_probs == None:
+    percZeros = (D == 0).sum().sum() / (D.shape[0]*D.shape[1])
+    NB_probs = 1-percZeros
+    print(f"Data is {percZeros*100:.2f}% sparse")
+
+if useTensorboard:
+    from torch.utils.tensorboard import SummaryWriter
+    writer = SummaryWriter(comment = tensorboard_identifier)
+
+
+# Instantiate the model
+model = GammaMatrixFactorization(D.shape[1], num_patterns, D.shape[0], NB_probs = NB_probs, device=device)
+
+# Draw model
+if draw_model != None:
+    pyro.render_model(model, model_args=(D,),
+                     render_params=True,
+                     render_distributions=True,
+                     #render_deterministic=True,
+                     filename=draw_model)
+
+
+# Instantiate the guide
+guide = AutoNormal(model)
+
+# Define the inference algorithm
+svi = pyro.infer.SVI(model=model,
+                    guide=guide,
+                    optim=optimizer,
+                    loss=loss_fn)
+
+
+# Start timer
+startTime = datetime.now()
+
+steps = []
+losses = []
+
+# Run inference
+for step in range(1,num_steps+1):
+    loss = svi.step(D)
+
+    if step % 10 == 0:
+        if useTensorboard:
+            writer.add_scalar("Loss/train", loss, step)
+            writer.flush()
+
+        losses.append(loss)
+        steps.append(step)
+
+    if step % 50 == 0:
+        if useTensorboard:
+            plot_grid(pyro.param("loc_P").detach().to('cpu').numpy(), coords, plot_dims[0], plot_dims[1], savename = None)
+            writer.add_figure("loc_P", plt.gcf(), step)
+
+            plt.hist(pyro.param("loc_P").detach().to('cpu').numpy().flatten(), bins=30)
+            writer.add_figure("loc_P_hist", plt.gcf(), step)
+
+            plt.hist(pyro.param("loc_A").detach().to('cpu').numpy().flatten(), bins=30)
+            writer.add_figure("loc_A_hist", plt.gcf(), step)
+
+            plt.hist(pyro.param("scale_P").detach().to('cpu').numpy().flatten(), bins=30)
+            writer.add_figure("scale_P_hist", plt.gcf(), step)
+
+            plt.hist(pyro.param("scale_A").detach().to('cpu').numpy().flatten(), bins=30)
+            writer.add_figure("scale_A_hist", plt.gcf(), step)
+
+    if step % 100 == 0:
+
+        print(f"Iteration {step}, ELBO loss: {loss}")
+
+        if useTensorboard:
+            D_reconstructed = model.D_reconstructed.detach().cpu().numpy()
+            plt.hist(D_reconstructed.flatten(), bins=30)
+            writer.add_figure("D_reconstructed_his", plt.gcf(), step)
+
+endTime = datetime.now()
+print('Runtime: '+ str(round((endTime - startTime).total_seconds())) + ' seconds')
+
+# Save the inferred parameters
+if not os.path.exists(outputDir):
+    os.makedirs(outputDir)
+
+#savename = '/disk/kyla/projects/pyro_NMF/results/scaleD_constraint_comparison/' + identifier + '/' + 'ISO_n12'+ identifier
+result_anndata = data.copy()
+
+loc_A = pd.DataFrame(pyro.param("loc_A").detach().to('cpu').numpy()).T
+loc_A.columns = ['Pattern_' + str(x+1) for x in loc_A.columns]
+loc_A.index = result_anndata.var.index
+result_anndata.varm['loc_A'] = loc_A
+print("Saving loc_A in anndata.varm['loc_A']")
+
+scale_A = pd.DataFrame(pyro.param("scale_A").detach().to('cpu').numpy()).T
+scale_A.columns = ['Pattern_' + str(x+1) for x in scale_A.columns]
+scale_A.index = result_anndata.var.index # need names to match anndata names
+result_anndata.varm['scale_A'] = scale_A
+print("Saving scale_A in anndata.varm['scale_A']")
+
+loc_P = pd.DataFrame(pyro.param("loc_P").detach().to('cpu').numpy())
+loc_P.columns = ['Pattern_' + str(x+1) for x in loc_P.columns]
+loc_P.index = result_anndata.obs.index
+result_anndata.obsm['loc_P'] = loc_P
+print("Saving loc_P in anndata.obsm['loc_P']")
+
+scale_P = pd.DataFrame(pyro.param("scale_P").detach().to('cpu').numpy())
+scale_P.columns = ['Pattern_' + str(x+1) for x in scale_P.columns]
+scale_P.index = result_anndata.obs.index
+result_anndata.obsm['scale_P'] = scale_P
+print("Saving scale_P in anndata.obsm['scale_P']")
+
+loc_D = pd.DataFrame(model.D_reconstructed.detach().cpu().numpy())
+loc_D.index = result_anndata.obs.index
+loc_D.columns = result_anndata.var.index # need names to match anndata names
+result_anndata.layers['loc_D'] = loc_D
+print("Saving loc_D in anndata.layers['loc_D']")
+
+plot_grid(pyro.param("loc_P").detach().to('cpu').numpy(), coords, plot_dims[0], plot_dims[1], savename = savename + "_loc_P.pdf")
+
+result_anndata.uns['runtime (seconds)'] = round((endTime - startTime).total_seconds())
+result_anndata.uns['loss'] = pd.DataFrame(losses, index=steps, columns=['loss'])
+
+result_anndata.write_h5ad(outputDir + savename + '.h5ad')
+
+if useTensorboard:
+    writer.flush()
+
+#
